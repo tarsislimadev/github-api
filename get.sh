5e07fab0d283f191b470373a07c52ee2e5bd163d
Tarsis <br.tmvdl@gmail.com>
2023-10-20 16:47:28 -0300

    Updating script files

--- get.sh
+++ get.sh
@@ -6,4 +6,4 @@ path="${1}"
 
 queries="${2}"
 
-curl -i -X GET "https://api.github.com/${path}?${queries}"
+curl -i -X GET -sL "Accept: application/vnd.github+json" -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/${path}?${queries}"
